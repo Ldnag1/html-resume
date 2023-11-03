073590d4f215e306741b687c37a5c25ece12f90a
Ldnag1 <77507825+Ldnag1@users.noreply.github.com>
2023-11-03 11:03:46 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://blahblahblah.danglandon2007.workers.dev",
+  VITE_TMDB_READ_API_KEY: "1af4e8b66b79a0e83ba24c13a2c9185f"
+};
