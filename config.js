81fc7c0ae07de875cb0a3391a2e4ffeb57d28d8c
Ldnag1 <77507825+Ldnag1@users.noreply.github.com>
2023-11-14 20:20:34 -0500

    update #1
    blah balh blah

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://blahblahblah.danglandon2007.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWY0ZThiNjZiNzlhMGU4M2JhMjRjMTNhMmM5MTg1ZiIsInN1YiI6IjY1MzUzYjNjYzE0ZmVlMDExZGU4MGRjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ZbiOK00D1k278RuZJxhvzNHB3f9m1ED3_omF2IC2jY"
+};
